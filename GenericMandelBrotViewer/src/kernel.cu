bdeccfaa2e29b835baea71f6a8fa730dc912a0e1
Teun <vanwezelteun@gmail.com>
2021-01-13 00:29:57 +0100

    Continued work on Mandelbrot Set Viewer
    - Optimized the iteration kernel for a 13% speedup.
    - Fixed the HSV to RGB conversion algorithm; it was causing problems with coloring.
    - Added smooth coloring.
    - Split the coloring of the iterated points into a separate kernel.
    - Added "debugging mode" where framerate is capped at 2.
    - Added keyboard controls for `maximum_iterations`.
    - Reduce calls to printf() for a less bloated terminal.

--- GenericMandelBrotViewer/src/kernel.cu
+++ GenericMandelBrotViewer/src/kernel.cu
@@ -5879,9 +5879,46 @@ extern "C" {
 #include <thrust/complex.h>
 #include <iostream>
 
-// TODO: take center_x and center_y variables into account
-__global__ void build_complex_grid_kernel(double center_x, double center_y, double draw_radius, int resolution_x, int resolution_y, thrust::complex<double>* points, thrust::complex<double>* iterated_points) {
-	int thread_index = threadIdx.x;
+// Debugging:
+#include <windows.h>
+bool debugging_enabled = true;
+
+// Define starting parameters for the mandelbrot
+double center_x = 0.0;
+double center_y = 0.0;
+int resolution_x = 1024;
+int resolution_y = 1024;
+double draw_radius = 2.5;
+double escape_radius_squared = 4; // escape_radius = 2^7 = 256
+int max_iterations = 64;
+
+bool incremental_iteration = false;
+int iterations_per_frame; // value set in main()
+int incremental_iterations_per_frame = 4;
+int rendered_iterations = 0;
+
+// Cuda parameters:
+int blockSize = 256;
+int numBlocks = int(ceil(resolution_x * resolution_y / blockSize));
+
+// Define variables used to store values for each pixel
+thrust::complex<double>* points;
+thrust::complex<double>* iterated_points;
+double* squared_absolute_values;
+unsigned char* pixels_rgb;
+unsigned short* iterationsArr;
+
+
+__global__ void build_complex_grid_kernel(
+    double center_x, double center_y, 
+    double draw_radius, 
+    int resolution_x, int resolution_y, 
+    thrust::complex<double>* points, 
+    thrust::complex<double>* iterated_points) 
+{
+    // Create a grid of complex numbers around the center point (center_x, center_y).
+    
+    int thread_index = threadIdx.x;
 	int thread_stride = blockDim.x;
 
 	double step_y = 2 * draw_radius / resolution_y;
@@ -5903,17 +5940,63 @@ __global__ void build_complex_grid_kernel(double center_x, double center_y, doub
 	}
 }
 
-
-__global__ void mandelbrot_iterate_and_color_kernel(
-    int max_iterations, 
-    double escape_radius_squared, 
-    int resolution_x, int resolution_y, 
-    thrust::complex<double>* points, 
-    thrust::complex<double>* iterated_points, 
-    double* squared_absolute_values, 
-    unsigned char* rgb_data
-) 
+__global__ void mandelbrot_iterate_kernel(
+    int max_iterations,
+    double escape_radius_squared,
+    int resolution_x, int resolution_y,
+    thrust::complex<double>* points,
+    thrust::complex<double>* iterated_points,
+    double* squared_absolute_values,
+    unsigned short* iterationsArr
+)
 {
+    int block_index_x = blockIdx.x;
+    int block_stride_x = gridDim.x;
+    //int block_index_y = blockIdx.y;
+    //int block_stride_y = gridDim.y;
+
+    int thread_index_x = threadIdx.x;
+    int thread_stride_x = blockDim.x;
+    //int thread_index_y = threadIdx.y;
+    //int thread_stride_y = blockDim.y;
+    int index;
+
+    //printf("thread_index_x: %i | block_index_x: %i | thread_stride_x: %i | block_stride_x: %i\n", thread_index_x, block_index_x, thread_stride_x, block_stride_x);
+    for (int pixel_y = block_index_x; pixel_y < resolution_y; pixel_y += block_stride_x)
+    {
+        for (int pixel_x = thread_index_x; pixel_x < resolution_x; pixel_x += thread_stride_x)
+        {
+            // Calculate the iterations required for a given point to exceed the escape radius.
+            index = pixel_y * resolution_y + pixel_x;
+            thrust::complex<double> c = points[index];
+            thrust::complex<double> it_point = iterated_points[index];
+            double sq_abs = squared_absolute_values[index];
+            unsigned short iterations_ = iterationsArr[index];
+            while (iterations_ < max_iterations && sq_abs < escape_radius_squared) {
+                it_point = thrust::complex<double>(it_point.real() * it_point.real() - it_point.imag() * it_point.imag(), 2 * it_point.real() * it_point.imag()) + c; // z^2 + c
+                sq_abs = it_point.real() * it_point.real() + it_point.imag() * it_point.imag();
+                iterations_++;
+            }
+            iterated_points[index] = it_point;
+            iterationsArr[index] = iterations_;
+            squared_absolute_values[index] = sq_abs;
+        }
+    }
+}
+
+
+__global__ void color_kernel(
+    int max_iterations,
+    unsigned short* iterationsArr,
+    double * squared_absolute_values,
+    int resolution_x,
+    int resolution_y,
+    double draw_radius,
+    unsigned char * rgb_data
+)
+{
+    // Do some coloring! 
+
     int block_index_x = blockIdx.x;
     int block_stride_x = gridDim.x;
     //int block_index_y = blockIdx.y;
@@ -5932,84 +6015,147 @@ __global__ void mandelbrot_iterate_and_color_kernel(
         for (int pixel_x = thread_index_x; pixel_x < resolution_x; pixel_x += thread_stride_x)
         {
             // Calculate the iterations required for a given point to exceed the escape radius.
-            iterations = 0;
             index = pixel_y * resolution_y + pixel_x;
-            // TODO: Optimize
-            while (iterations < max_iterations && squared_absolute_values[index] < escape_radius_squared) {
-                iterated_points[index] = iterated_points[index] * iterated_points[index] + points[index]; // z^2 + c
-                squared_absolute_values[index] = iterated_points[index].real() * iterated_points[index].real() + iterated_points[index].imag() * iterated_points[index].imag();
-                iterations++;
-            }
-            
-            // Coloring 
+            iterations = iterationsArr[index];
             if (iterations == max_iterations)
             {
-                rgb_data[3 * (resolution_y * pixel_y + pixel_x) + 0] = 0; // Red value
-                rgb_data[3 * (resolution_y * pixel_y + pixel_x) + 1] = 0; // Green value
-                rgb_data[3 * (resolution_y * pixel_y + pixel_x) + 2] = 0; // Blue value
-            }
+                // Values that don't escape are colored black:
+                rgb_data[3 * index + 0] = 25; // Red value
+                rgb_data[3 * index + 1] = 25; // Green value
+                rgb_data[3 * index + 2] = 25; // Blue value
+            } 
             else
             {
-                unsigned char red = iterations*iterations + 255 / squared_absolute_values[index];
-                // cap to 255
-                if (red > 255)
-                    red = 255;
-                else if (red < 0)
-                    red = 0;
-                unsigned char green = 0;
-                unsigned char blue = 0;
-                rgb_data[3 * (resolution_y * pixel_y + pixel_x) + 0] = red; // Red value
-                rgb_data[3 * (resolution_y * pixel_y + pixel_x) + 1] = green; // Green value
-                rgb_data[3 * (resolution_y * pixel_y + pixel_x) + 2] = blue; // Blue value
+                // Grayscale Coloring
+                float f_iterations = (float)iterations;
+                float f_max_iterations = (float)max_iterations;
+                unsigned char red;
+                unsigned char green;
+                unsigned char blue;
+                float escape_size = __double2float_rn(squared_absolute_values[index]);
+                float smoothed_iterations = iterations + 1 - log2f(log(escape_size)) + sqrtf(sqrtf(draw_radius));
+                float H = 360*smoothed_iterations / f_max_iterations;
+                float S = .65;
+                float V = 1;
+
+
+#pragma region HSV_to_RGB_Conversion
+                // HSV to RGB conversion, yay!
+                // TODO: look into edge cases for H and why they happen.
+                //if (H > 360 || H < 0 || S > 1 || S < 0 || V > 1 || V < 0)
+                //{
+                    //printf("x");
+                    //printf("The given HSV values are not in valid range.\n H: %f S: %.2f, V: %.2f\n", H, S, V);
+                    //printf("Iterations: %f\n", f_iterations);
+                //}
+                float h = H / 60;
+                float C = S * V;
+                float X = C * (1 - fabsf((fmodf(h, 2) - 1)));
+                float m = V - C;
+                float r, g, b;
+                if (h >= 0 && h <= 1)
+                {
+                    r = C;
+                    g = X;
+                    b = 0;
+                }
+                else if (h > 1 && h < 2)
+                {
+                    r = X;
+                    g = C;
+                    b = 0;
+                }
+                else if (h > 2 && h <= 3)
+                {
+                    r = 0;
+                    g = C;
+                    b = X;
+                }
+                else if (h > 3 && h <= 4)
+                {
+                    r = 0;
+                    g = X;
+                    b = C;
+                }
+                else if (h > 4 && h <= 5)
+                {
+                    r = X;
+                    g = 0;
+                    b = C;
+                }
+                else if (h > 5 && h <= 6)
+                {
+                    r = C;
+                    g = 0;
+                    b = X;
+                }
+                else // color white to make stand out 
+                {
+                    r = 1-m;
+                    g = 1-m;
+                    b = 1-m;
+                }
+                red = (r + m) * 255;
+                green = (g + m) * 255;
+                blue = (b + m) * 255;
+                // End of conversion.  
+#pragma endregion
+
+
+                // Cap RGB values to 255
+                if (red > 255) { red = 255; }
+                if (green > 255) { green = 255; }
+                if (blue > 255) { blue = 255; }
+
+                rgb_data[3 * index + 0] = red; // Red value
+                rgb_data[3 * index + 1] = green; // Green value
+                rgb_data[3 * index + 2] = blue; // Blue value
             }
-                    }
+        }
     }
 }
 
-
-// Define starting parameters for the mandelbrot
-double center_x = 0.0;
-double center_y = 0.0;
-int resolution_x = 1024;
-int resolution_y = 1024;
-double draw_radius = 2.5;
-double escape_radius_squared = 4; // escape_radius = 2
-int max_iterations = 1024;
-
-bool incremental_iteration = false;
-int iterations_per_frame; // value set in main()
-int incremental_iterations_per_frame = 4;
-int rendered_iterations = 0;
-
-
-
-// Cuda parameters:
-int blockSize = 256;
-int numBlocks = int(ceil(resolution_x * resolution_y / blockSize));
-
-//// Define variables used to store values for each pixel
-
-thrust::complex<double>* points;
-thrust::complex<double>* iterated_points;
-double* squared_absolute_values;
-unsigned char* pixels_rgb;
 
 // GLFW
 GLFWwindow* window;
 
 void host_build_complex_grid_kernel()
 {
-    build_complex_grid_kernel << < 1, 1024 >> > (center_x, center_y, draw_radius, resolution_x, resolution_y, points, iterated_points);
+    build_complex_grid_kernel <<< 1, 1024 >>> (center_x, center_y, draw_radius, resolution_x, resolution_y, points, iterated_points);
 }
 
 void host_mandelbrot_iterate_and_color()
 {
-    mandelbrot_iterate_and_color_kernel << < numBlocks, blockSize >> > (max_iterations, escape_radius_squared, resolution_x, resolution_y, points, iterated_points, squared_absolute_values, pixels_rgb);
+
+    mandelbrot_iterate_kernel <<< numBlocks, blockSize
+        >>> (
+            max_iterations,
+            escape_radius_squared,
+            resolution_x, resolution_y,
+            points,
+            iterated_points,
+            squared_absolute_values,
+            iterationsArr
+            );
+
+    cudaDeviceSynchronize();
+    color_kernel <<< numBlocks, blockSize
+    >>> (
+            max_iterations,
+            iterationsArr,
+            squared_absolute_values,
+            resolution_x,
+            resolution_y,
+            draw_radius,
+            pixels_rgb
+        );
 }
 
+// Under maintenance.
 void host_mandelbrot_iterate_n_and_color(int iterations)
 {
-    mandelbrot_iterate_and_color_kernel << < numBlocks, blockSize >> > (iterations, escape_radius_squared, resolution_x, resolution_y, points, iterated_points, squared_absolute_values, pixels_rgb);
+    host_mandelbrot_iterate_and_color();
+    //mandelbrot_iterate_and_color_kernel << < numBlocks, blockSize >> > (iterations, escape_radius_squared, resolution_x, resolution_y, points, iterated_points, squared_absolute_values, pixels_rgb);
 }
 
 
@@ -6020,8 +6166,10 @@ void reset_render_objects()
     // Rebuild the grid of complex numbers based on (new) center and (new) center.
     host_build_complex_grid_kernel(); 
     // Reset the `squared_absolute_values` to zero by allocating the memory space again.
-    cudaFree(squared_absolute_values); 
+    cudaFree(squared_absolute_values);
+    cudaFree(iterationsArr);
     cudaMallocManaged(&squared_absolute_values, resolution_x * resolution_y * sizeof(double));
+    cudaMallocManaged(&iterationsArr, resolution_x * resolution_y * sizeof(unsigned short));
     // Synchronize the GPU so the whole thing doesn't crash.
     cudaDeviceSynchronize();
     // Reset the amount of rendered iterations to 0. 
@@ -6029,12 +6177,20 @@ void reset_render_objects()
 }
 
 
-
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (action == GLFW_PRESS)
     {
         switch (key) {
+        case GLFW_KEY_D:
+            if (debugging_enabled) {
+                debugging_enabled = false;
+            }
+            else {
+                debugging_enabled = true;
+            }
+            reset_render_objects();
+            break;
         case GLFW_KEY_EQUAL: // zoom in, = is also +
             draw_radius *= 0.75; // zoom in
             reset_render_objects();
@@ -6067,10 +6223,24 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
             break;
         case GLFW_KEY_LEFT_BRACKET:
             max_iterations *= 0.9;
+            printf("Max iterations now at: %d\n", max_iterations);
+            if (incremental_iteration) {
+                iterations_per_frame = incremental_iterations_per_frame;
+            }
+            else {
+                iterations_per_frame = max_iterations;
+            }
             reset_render_objects();
             break;
         case GLFW_KEY_RIGHT_BRACKET:
             max_iterations /= 0.9;
+            printf("Max iterations now at: %d\n", max_iterations);
+            if (incremental_iteration) {
+                iterations_per_frame = incremental_iterations_per_frame;
+            }
+            else {
+                iterations_per_frame = max_iterations;
+            }
             reset_render_objects();
             break;
         case GLFW_KEY_I:
@@ -6106,10 +6276,13 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 
 int main() {
 
+    printf("numBlocks: %d\nblockSize: %d\n", numBlocks, blockSize);
+
     cudaMallocManaged(&points, resolution_x * resolution_y * sizeof(thrust::complex<double>));
     cudaMallocManaged(&iterated_points, resolution_x * resolution_y * sizeof(thrust::complex<double>));
     cudaMallocManaged(&squared_absolute_values, resolution_x * resolution_y * sizeof(double));
     cudaMallocManaged(&pixels_rgb, resolution_x * resolution_y * 3 * sizeof(unsigned char));
+    cudaMallocManaged(&iterationsArr, resolution_x * resolution_y * sizeof(unsigned short));
 
     host_build_complex_grid_kernel();
     cudaDeviceSynchronize();
@@ -6149,12 +6322,18 @@ int main() {
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        if (debugging_enabled)
+        {
+            Sleep(500); // set max fps to 2
+        }
+
         /* Render here */
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // TODO: make it so that the iterations increase for as long as the center and draw_radius are the same - up to the max of course
         if (rendered_iterations < max_iterations) {
+            printf("rendering %d iterations\n", iterations_per_frame);
             host_mandelbrot_iterate_n_and_color(iterations_per_frame);
             cudaDeviceSynchronize();
             rendered_iterations += iterations_per_frame;
@@ -6175,7 +6354,7 @@ int main() {
     cudaFree(iterated_points);
     cudaFree(squared_absolute_values);
     cudaFree(pixels_rgb);
+    cudaFree(iterationsArr);
 
 	return 0;
 }
-
